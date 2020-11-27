fea329a552691a8abcfc9729f59c1c0f0dbc84eb
elitah <elitah@users.noreply.github.com>
2020-11-27 19:26:18 +0800

    Update liveconfig.go

--- configure/liveconfig.go
+++ configure/liveconfig.go
@@ -105,7 +105,7 @@ func init() {
 	pflag.Int("read_timeout", 10, "read time out")
 	pflag.Int("write_timeout", 10, "write time out")
 	pflag.Int("gop_num", 1, "gop num")
-	pflag.Parse()
+	//pflag.Parse()
 	Config.BindPFlags(pflag.CommandLine)
 
 	// File
